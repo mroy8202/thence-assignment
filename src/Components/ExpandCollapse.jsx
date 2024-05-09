ea63929e73e08e68945504cc5111d523dd56b37b
mritunjayRoy <mroy8202@gmail.com>
2024-05-09 14:17:05 +0530

    Section2 added

---
+++ src/Components/ExpandCollapse.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Plus from '../Assets/plus.svg'
+import Minus from '../Assets/minus.svg'
+
+const ExpandCollapse = ({headingText, bottomText}) => {
+
+    const [expanded, setExpanded] = useState(false)
+
+    const toggleExpand = () => {
+        setExpanded(!expanded);
+    }
+
+  return (
+    <div className='flex-flex-col gap-2'>
+        <div className='flex justify-between cursor-pointer' onClick={toggleExpand}>
+            <p className='font-semibold text-xl text-[#1C1C1C] w-[70%]'>
+                {headingText}
+            </p>
+            <div className='w-[30%] flex items-center justify-end'>
+                <img src={expanded ? Minus : Plus} alt='img' 
+                    className='h-[14px] w-[14px] '
+                />
+            </div>
+        </div>
+        {expanded && (
+                <div className='text-[#617275] text-base'>
+                    {bottomText} {/* Use props.bottomText */}
+                </div>
+        )}
+    </div>
+  )
+}
+
+export default ExpandCollapse
--- src/Components/Section2.jsx
+++ src/Components/Section2.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Union from '../Assets/Union.svg'
+import ExpandCollapse from './ExpandCollapse'
 
 const Section2 = () => {
   return (
-    <div className='mt-32 bg-[#E8EEE7] rounded-3xl h-[500px]'>
+    <div className='mt-32 bg-[#E8EEE7] rounded-3xl h-[500px] flex flex-row gap-8'>
         <div className='w-[50%] flex flex-col relative'>
             <div className='ml-16 mt-16 mb-16'>
                 <div className='font-grace font-normal text-2xl text-[#9E9D9D]'>
@@ -21,7 +22,45 @@ const Section2 = () => {
             </div>
         </div>
 
-        <div></div>
+        <div className='w-[50%] border border-black flex items-center justify-center'>
+            <div className='flex flex-col gap-4 w-[80%]'>
+                <div className='flex-flex-col gap-2'>
+                    <ExpandCollapse 
+                        headingText={"Do you offer freelancers?"}
+                        bottomText={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+                    />
+                </div>
+
+                <div className='flex-flex-col gap-2'>
+                    <ExpandCollapse 
+                        headingText={"What’s the guarantee that I will be satisfied with the hired talent?"}
+                        bottomText={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+                    />
+                </div>
+
+                <div className='flex-flex-col gap-2'>
+                    <ExpandCollapse 
+                        headingText={"Can I hire multiple talents at once?"}
+                        bottomText={"If unhappy with a project, communicate with the freelancer, allow for revisions, and refer to the agreement. Escalate to platform support if needed, considering mediation. Review policies, seek collaborative solutions for resolution."}
+                    />
+                </div>
+
+                <div className='flex-flex-col gap-2'>
+                    <ExpandCollapse 
+                        headingText={"Why should I not go to an agency directly?"}
+                        bottomText={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+                    />
+                </div>
+
+                <div className='flex-flex-col gap-2'>
+                    <ExpandCollapse 
+                        headingText={"Who can help me pick a right skillset and duration for me?"}
+                        bottomText={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+                    />
+                </div>
+
+            </div>
+        </div>
     </div>
   )
 }
